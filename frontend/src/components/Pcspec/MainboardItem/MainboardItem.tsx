1b137ee9288a9a4c9c238649ee40124a2e606ad7
ratchapon-p <ratchapon-p@rmutp.ac.th>
2023-11-28 16:53:45 +0700

    vganmb-items

--- frontend/src/components/Pcspec/MainboardItem/MainboardItem.tsx
+++ frontend/src/components/Pcspec/MainboardItem/MainboardItem.tsx
@@ -61,6 +61,162 @@ const MainboardItem = () => {
                 <Link to='/' className='link-to-item-details'>รายละเอียด</Link>
             </div>
         </div>
+        
+        <div className='cpuItem-details'>
+            <div className='cpuItem-details-text'>
+                <div className='cpuItem-brand'>ASROCK</div>
+                <div className='cpuItem-brand-details'>ASROCK B450M STEEL LEGEND</div>
+            </div>
+            <div className='cpuItem-images'><img src={mainboard1} className='cpuItem-images-item'/></div>
+            <div className='cpuItem-price-details'><p className='cpu-item-price-text'>ราคา</p><h5 className='cpuItem-price'>{'2,990.-'}</h5></div>
+            <div className='add-to-pcspec'>
+                <button className='add-to-pcspec-btn'>จัดชุดสเปค</button>
+                <Link to='/' className='link-to-item-details'>รายละเอียด</Link>
+            </div>
+        </div>
+
+        <div className='cpuItem-details'>
+            <div className='cpuItem-details-text'>
+                <div className='cpuItem-brand'>ASROCK</div>
+                <div className='cpuItem-brand-details'>ASROCK B450 STEEL LEGEND (DDR4) (SOCKET AM4) (ATX)</div>
+            </div>
+            <div className='cpuItem-images'><img src={mainboard2} className='cpuItem-images-item'/></div>
+            <div className='cpuItem-price-details'><p className='cpu-item-price-text'>ราคา</p><h5 className='cpuItem-price'>{'2,490.-'}</h5></div>
+            <div className='add-to-pcspec'>
+                <button className='add-to-pcspec-btn'>จัดชุดสเปค</button>
+                <Link to='/' className='link-to-item-details'>รายละเอียด</Link>
+            </div>
+        </div>
+
+        <div className='cpuItem-details'>
+            <div className='cpuItem-details-text'>
+                <div className='cpuItem-brand'>ASUS</div>
+                <div className='cpuItem-brand-details'>ASUS PRIME A320M-K (DDR4) (SOCKET AM4) (MICRO-ATX)</div>
+            </div>
+            <div className='cpuItem-images'><img src={mainboard3} className='cpuItem-images-item'/></div>
+            <div className='cpuItem-price-details'><p className='cpu-item-price-text'>ราคา</p><h5 className='cpuItem-price'>{'1,890.-'}</h5></div>
+            <div className='add-to-pcspec'>
+                <button className='add-to-pcspec-btn'>จัดชุดสเปค</button>
+                <Link to='/' className='link-to-item-details'>รายละเอียด</Link>
+            </div>
+        </div>
+
+        <div className='cpuItem-details'>
+            <div className='cpuItem-details-text'>
+                <div className='cpuItem-brand'>ASUS</div>
+                <div className='cpuItem-brand-details'>ASUS PRIME H610M-E D4-CSM (DDR4) (SOCKET LGA 1700) (MICRO-ATX)</div>
+            </div>
+            <div className='cpuItem-images'><img src={mainboard4} className='cpuItem-images-item'/></div>
+            <div className='cpuItem-price-details'><p className='cpu-item-price-text'>ราคา</p><h5 className='cpuItem-price'>{'2,850.-'}</h5></div>
+            <div className='add-to-pcspec'>
+                <button className='add-to-pcspec-btn'>จัดชุดสเปค</button>
+                <Link to='/' className='link-to-item-details'>รายละเอียด</Link>
+            </div>
+        </div>
+        
+        <div className='cpuItem-details'>
+            <div className='cpuItem-details-text'>
+                <div className='cpuItem-brand'>ASROCK</div>
+                <div className='cpuItem-brand-details'>ASROCK B450M STEEL LEGEND</div>
+            </div>
+            <div className='cpuItem-images'><img src={mainboard1} className='cpuItem-images-item'/></div>
+            <div className='cpuItem-price-details'><p className='cpu-item-price-text'>ราคา</p><h5 className='cpuItem-price'>{'2,990.-'}</h5></div>
+            <div className='add-to-pcspec'>
+                <button className='add-to-pcspec-btn'>จัดชุดสเปค</button>
+                <Link to='/' className='link-to-item-details'>รายละเอียด</Link>
+            </div>
+        </div>
+
+        <div className='cpuItem-details'>
+            <div className='cpuItem-details-text'>
+                <div className='cpuItem-brand'>ASROCK</div>
+                <div className='cpuItem-brand-details'>ASROCK B450 STEEL LEGEND (DDR4) (SOCKET AM4) (ATX)</div>
+            </div>
+            <div className='cpuItem-images'><img src={mainboard2} className='cpuItem-images-item'/></div>
+            <div className='cpuItem-price-details'><p className='cpu-item-price-text'>ราคา</p><h5 className='cpuItem-price'>{'2,490.-'}</h5></div>
+            <div className='add-to-pcspec'>
+                <button className='add-to-pcspec-btn'>จัดชุดสเปค</button>
+                <Link to='/' className='link-to-item-details'>รายละเอียด</Link>
+            </div>
+        </div>
+
+        <div className='cpuItem-details'>
+            <div className='cpuItem-details-text'>
+                <div className='cpuItem-brand'>ASUS</div>
+                <div className='cpuItem-brand-details'>ASUS PRIME A320M-K (DDR4) (SOCKET AM4) (MICRO-ATX)</div>
+            </div>
+            <div className='cpuItem-images'><img src={mainboard3} className='cpuItem-images-item'/></div>
+            <div className='cpuItem-price-details'><p className='cpu-item-price-text'>ราคา</p><h5 className='cpuItem-price'>{'1,890.-'}</h5></div>
+            <div className='add-to-pcspec'>
+                <button className='add-to-pcspec-btn'>จัดชุดสเปค</button>
+                <Link to='/' className='link-to-item-details'>รายละเอียด</Link>
+            </div>
+        </div>
+
+        <div className='cpuItem-details'>
+            <div className='cpuItem-details-text'>
+                <div className='cpuItem-brand'>ASUS</div>
+                <div className='cpuItem-brand-details'>ASUS PRIME H610M-E D4-CSM (DDR4) (SOCKET LGA 1700) (MICRO-ATX)</div>
+            </div>
+            <div className='cpuItem-images'><img src={mainboard4} className='cpuItem-images-item'/></div>
+            <div className='cpuItem-price-details'><p className='cpu-item-price-text'>ราคา</p><h5 className='cpuItem-price'>{'2,850.-'}</h5></div>
+            <div className='add-to-pcspec'>
+                <button className='add-to-pcspec-btn'>จัดชุดสเปค</button>
+                <Link to='/' className='link-to-item-details'>รายละเอียด</Link>
+            </div>
+        </div>
+        
+        <div className='cpuItem-details'>
+            <div className='cpuItem-details-text'>
+                <div className='cpuItem-brand'>ASROCK</div>
+                <div className='cpuItem-brand-details'>ASROCK B450M STEEL LEGEND</div>
+            </div>
+            <div className='cpuItem-images'><img src={mainboard1} className='cpuItem-images-item'/></div>
+            <div className='cpuItem-price-details'><p className='cpu-item-price-text'>ราคา</p><h5 className='cpuItem-price'>{'2,990.-'}</h5></div>
+            <div className='add-to-pcspec'>
+                <button className='add-to-pcspec-btn'>จัดชุดสเปค</button>
+                <Link to='/' className='link-to-item-details'>รายละเอียด</Link>
+            </div>
+        </div>
+
+        <div className='cpuItem-details'>
+            <div className='cpuItem-details-text'>
+                <div className='cpuItem-brand'>ASROCK</div>
+                <div className='cpuItem-brand-details'>ASROCK B450 STEEL LEGEND (DDR4) (SOCKET AM4) (ATX)</div>
+            </div>
+            <div className='cpuItem-images'><img src={mainboard2} className='cpuItem-images-item'/></div>
+            <div className='cpuItem-price-details'><p className='cpu-item-price-text'>ราคา</p><h5 className='cpuItem-price'>{'2,490.-'}</h5></div>
+            <div className='add-to-pcspec'>
+                <button className='add-to-pcspec-btn'>จัดชุดสเปค</button>
+                <Link to='/' className='link-to-item-details'>รายละเอียด</Link>
+            </div>
+        </div>
+
+        <div className='cpuItem-details'>
+            <div className='cpuItem-details-text'>
+                <div className='cpuItem-brand'>ASUS</div>
+                <div className='cpuItem-brand-details'>ASUS PRIME A320M-K (DDR4) (SOCKET AM4) (MICRO-ATX)</div>
+            </div>
+            <div className='cpuItem-images'><img src={mainboard3} className='cpuItem-images-item'/></div>
+            <div className='cpuItem-price-details'><p className='cpu-item-price-text'>ราคา</p><h5 className='cpuItem-price'>{'1,890.-'}</h5></div>
+            <div className='add-to-pcspec'>
+                <button className='add-to-pcspec-btn'>จัดชุดสเปค</button>
+                <Link to='/' className='link-to-item-details'>รายละเอียด</Link>
+            </div>
+        </div>
+
+        <div className='cpuItem-details'>
+            <div className='cpuItem-details-text'>
+                <div className='cpuItem-brand'>ASUS</div>
+                <div className='cpuItem-brand-details'>ASUS PRIME H610M-E D4-CSM (DDR4) (SOCKET LGA 1700) (MICRO-ATX)</div>
+            </div>
+            <div className='cpuItem-images'><img src={mainboard4} className='cpuItem-images-item'/></div>
+            <div className='cpuItem-price-details'><p className='cpu-item-price-text'>ราคา</p><h5 className='cpuItem-price'>{'2,850.-'}</h5></div>
+            <div className='add-to-pcspec'>
+                <button className='add-to-pcspec-btn'>จัดชุดสเปค</button>
+                <Link to='/' className='link-to-item-details'>รายละเอียด</Link>
+            </div>
+        </div>
 
     </div>
   )
